9c67492bca329ab554552be7a58c846e4c1f4d93
Adam Matthiesen <amatthiesen@outlook.com>
2023-08-23 10:48:41 -0700

    fixed spelling issue

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,5 +1,5 @@
 export const profile = {
   name: "Adam Matthiesen & Alox Leyva",
   title: "Hobbiest Photographers",
-  bio: "We both have a love for Photography which let to us creating our website to share our work, and offer our services to anyone interested! We do all sorts of different type of Photography!",
+  bio: "We both have a love for Photography which led to us creating our website to share our work, and offer our services to anyone interested! We do all sorts of different type of Photography!",
 }
